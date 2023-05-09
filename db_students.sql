81937f2c74b7bf2c87352a12441bfd970a5ce0a8
jmlloren08 <jaymarvinc.lloren@gmail.com>
2023-05-09 11:48:54 +0800

    1

--- db_students.sql
+++ db_students.sql
@@ -2,7 +2,7 @@
 --
 -- Host: 104.199.204.91    Database: db_students
 -- ------------------------------------------------------
--- Server version	5.7.40-google-log
+-- Server version	8.0.26-google
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -21,7 +21,7 @@ SET @@SESSION.SQL_LOG_BIN= 0;
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '8a7f1f55-ed55-11ed-a7de-42010a400002:1-15860';
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '8a7f1f55-ed55-11ed-a7de-42010a400002:1-17057';
 
 --
 -- Table structure for table `mytable`
@@ -31,14 +31,14 @@ DROP TABLE IF EXISTS `mytable`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `mytable` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `address` varchar(45) DEFAULT NULL,
   `birthdate` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,11 +59,11 @@ DROP TABLE IF EXISTS `tbluser`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `tbluser` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `user` varchar(45) DEFAULT NULL,
   `pass` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,4 +86,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-09 10:05:57
+-- Dump completed on 2023-05-09 11:48:14
