a0fc570c20bd036d812cd2d8d2810e11d69678fe
jmlloren08 <jaymarvinc.lloren@gmail.com>
2023-05-10 11:38:58 +0800

    1

--- conn.php
+++ conn.php
@@ -1,12 +1,24 @@
 <?php
 
-    $conn = mysqli_connect('104.199.204.91','root','','db_students');
-
-    if(!$conn){
-        die("Connection failed:" . mysqli_connect_error());
-    }else{
-        echo "Connected successfully";
+Class connString{
+    private $server = "mysql:unix_socket=/cloudsql/pwa-app-project-386102:asia-east1:pwa-app-project;port=3306;dbname=db_students";
+    private $username = "root";
+    private $password = "";
+    private $option = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+    protected $conn;
+    public function open(){
+        try{
+            $this->conn = new PDO($this->server, $this->username, $this->password, $this->option);
+            return $this->conn;
+        }
+        catch(PDOException $e){
+            echo "No connection received: " . $e->getMessage(); 
+        }
+        echo "Connected!";
     }
+    public function close(){
+        $this->conn = null;
+    }
+}
 
-    echo "Connected!";
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 
 <?php
 
-    $conn = mysqli_connect('104.199.204.91','root','','db_students');
-
-    if(!$conn){
-        die("Connection failed:" . mysqli_connect_error());
-    }else{
-        echo "Connected successfully";
-    }
-
-    echo "Connected!";
+   include('conn.php');
 ?>
 
 <!DOCTYPE html>
