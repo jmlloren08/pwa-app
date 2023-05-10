8da2c15865a580f0637c3d2eb531436d844076fa
jmlloren08 <jaymarvinc.lloren@gmail.com>
2023-05-10 11:13:05 +0800

    1

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 
-    $conn = mysqli_connect('mysql:unix_socket=/cloudsql/pwa-app-project-386102:asia-east1:pwa-app-project','root','','db_students');
+    $conn = mysqli_connect('104.199.204.91','root','','db_students');
 
     if(!$conn){
         die("Connection failed:" . mysqli_connect_error());
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+
+<?php
+
+    $conn = mysqli_connect('104.199.204.91','root','','db_students');
+
+    if(!$conn){
+        die("Connection failed:" . mysqli_connect_error());
+    }else{
+        echo "Connected successfully";
+    }
+
+    echo "Connected!";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -89,6 +103,9 @@
               <p class="m-0">Don't have an account? <a href="register.php"> Register</a>.</p>
             </div>
           </form>
+
+          <?php echo isset($error) ? $error : ""; ?>
+
         </div>
       </div>
     </div>
